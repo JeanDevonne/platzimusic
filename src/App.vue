2bfbbef4aec8975f620cdc18d38d481ec3e44237
Jean Leon <jean_leo2000@hotmail.com>
2019-04-17 13:02:14 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 PlatziMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+</template>
+
+<script>
+import Artist from './components/Artist.vue'
+import getArtists from './api'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        { name: 'Peru', value: 'peru' },
+        { name: 'Argentina', value: 'argentina' },
+        { name: 'Colombia', value: 'colombia' },
+        { name: 'España', value: 'spain' }
+      ],
+      selectedCountry: 'peru'
+    }
+  },
+  components: {
+    Artist
+  },
+  methods: {
+    refreshArtists() {
+      const self = this
+      getArtists(this.selectedCountry)
+        .then(function(artists) {
+          self.artists = artists
+        })
+    }
+  },
+  mounted() {
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry(){
+      this.refreshArtists()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,16 @@
+// Nombre de aplicación	platzimusic
+// Clave API	f553190e9836c34ef07de71688c16cef
+// Secreto compartido	7befb4d36b9e8dd8f6a88b3af2d42ff2
+// Registrado para	jdevonne
+
+// Application name	platzimusic
+// API key	e7412fba3fca2bfbb7c2a486d640d955
+// Shared secret	040510c8a6df5c851627935a9752d1d4
+// Registered to	jdevonne
+
+export default {
+    appName: 'platzimusic',
+    apiKey: 'f553190e9836c34ef07de71688c16cef',
+    secret: '7befb4d36b9e8dd8f6a88b3af2d42ff2',
+    registeredTo: 'jdevonne'
+}
